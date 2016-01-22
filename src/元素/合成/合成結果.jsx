2787fb6d056aff9dafb5b323ab5875fcb834f794
薛丞宏 <ihcaoe@gmail.com>
2016-01-22 12:07:26 +0800

    會當重載入無仝的音矣

--- src/元素/合成/合成結果.jsx
+++ src/元素/合成/合成結果.jsx
@@ -6,7 +6,6 @@ import superagent from 'superagent-bluebird-promise'
 import Debug from 'debug'
 
 var debug = Debug('ing7:合成結果')
-var base64 = require('base64-js')
 
 class 合成結果 extends React.Component {
 
@@ -20,22 +19,20 @@ class 合成結果 extends React.Component {
   render () {
   //  debug((this.props.查詢結果['合成結果']))
   //  debug(new Buffer(this.props.查詢結果['合成結果'],'binary').toString('base64'))
+    let 合成音檔=document.getElementById('合成音檔');
+    if (合成音檔)
+    {
+      合成音檔.load();
+     }
     return (
         <div className='main container'>
         <h3>合成結果：</h3>
-		<audio controls>
+		<audio controls autoPlay id="合成音檔">
 		  <source type="audio/wav"
 	  	   src={this.props.後端網址
 	  	     +'語音合成?查詢腔口='+this.props.腔口
 	  	     +'&查詢語句='+this.props.語句}/>
-		</audio> 
-        <h3>合成結果：</h3>
-		<audio controls>
-		  <source type="audio/wav"
-	  	   src={'data:audio/wav;base64,'
-	  	   +new Buffer(this.props.查詢結果['合成結果']).toString('base64') }/>
-		  Your browser does not support the audio element.
-		</audio> 
+		</audio>
         </div>
       )
   }
@@ -43,29 +40,5 @@ class 合成結果 extends React.Component {
 
 export default Transmit.createContainer(合成結果, {
   queries: {
-    查詢結果 ({後端網址, 腔口, 翻譯正規化結果}) {
-      if (後端網址===undefined)
-        return Promise.resolve({
-  	          '狀態': '連線中……',
-          })
-      return superagent.post(後端網址+'語音合成')
-  	    .set('Content-Type', 'application/x-www-form-urlencoded')
-  		  .send({
-  	          '查詢腔口': 腔口,
-  	          '查詢語句': 翻譯正規化結果,
-  	        })
-          .then(({text}) => ({
-  	          '狀態': '成功',
-  	          '查詢腔口': 腔口,
-  	          '查詢語句': 翻譯正規化結果,
-              '合成結果': text,
-          }))
-          .catch((err) => ({
-  	          '狀態': '失敗',
-  	          '查詢腔口': 腔口,
-  	          '查詢語句': 翻譯正規化結果,
-              '合成結果': '',
-          }))
-    }
   }
 })
