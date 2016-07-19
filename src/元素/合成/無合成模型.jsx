993bb2964b17175321477465a681736c97f9762e
薛丞宏 <ihcaoe@gmail.com>
2016-07-19 15:29:04 +0800

    改服務的版面

--- src/元素/合成/無合成模型.jsx
+++ src/元素/合成/無合成模型.jsx
@@ -12,7 +12,7 @@ export default class 無合成模型 extends React.Component {
     return (
         <div className=''>
           目前沒有族語的語音模型，歡迎一起來
-          <a href='https://g0v.hackpad.com/f4rSgcFTIzz'>hackpad</a>
+          <a href='https://g0v.hackpad.com/f4rSgcFTIzz'>我想幫忙</a>
           討論如何製作喲！！
         </div>
       );
