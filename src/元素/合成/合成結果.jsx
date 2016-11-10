51e31a52453fbd2af029a693d57205b25818fd7a
薛丞宏 <ihcaoe@gmail.com>
2016-11-10 12:45:48 +0800

    改用ref，較穩

--- src/元素/合成/合成結果.jsx
+++ src/元素/合成/合成結果.jsx
@@ -10,14 +10,14 @@ export default class 合成結果 extends React.Component {
     if (nextProps.後端網址 === this.props.後端網址 &&
       nextProps.腔口 === this.props.腔口 &&
       nextProps.語句 === this.props.語句) return;
-    let 音檔 = document.getElementById('合成音檔');
+    let 音檔 = this.refs.合成音檔;
     音檔.load();
   }
 
   render () {
     return (
         <div className=''>
-        <audio controls autoPlay id="合成音檔">
+        <audio controls autoPlay ref="合成音檔">
           <source type="audio/wav"
              src={this.props.後端網址
                + '語音合成?查詢腔口=' + this.props.腔口
